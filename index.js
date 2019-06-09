f9b335d42d5d9149fc667baab2dc9b9860b51754
Tinus Strydom <tinuss@loans-inc.co.za>
2019-06-09 14:12:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,7 +19,9 @@ function destructivelyRemoveFirstKitten(){
 }
 function appendKitten(name){
   kittens = [...kittens, name];
+  return kittens;
 }
 function prependKitten(name){
   kittens = [name, ...kittens];
+  return kittens;
 }
